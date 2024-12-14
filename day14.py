b7df5b80ee00c8250d5585dd381f326c85df5e3b
haris-hm <haris.mehuljic@drake.edu>
2024-12-14 12:14:08 -0600

    trying clustering

--- day14.py
+++ day14.py
@@ -86,16 +86,86 @@ class Map():
         
         return score
     
+    def promising(self, curr_char: str, 
+                  coord: tuple[int], 
+                  map_list: list[list[str]], 
+                  visited_nodes: set[tuple[int]]) -> bool:
+        y: int = coord[0]
+        x: int = coord[1]
 
+        within_bounds: bool = 0 <= y < len(map_list) and 0 <= x < len(map_list[0])
+        not_visited: bool = (y, x) not in visited_nodes
+        same_id: bool = False
+
+        if (within_bounds):
+            same_id = map_list[y][x] == curr_char
+
+        if (same_id and not_visited and within_bounds):
+            return True
+        else:
+            return False
     
-    def clustering(self, threshold: float) -> None:
+    def identify_cluster(self, starting_idx: tuple[int], 
+                         threshold: int, 
+                         visited_nodes: set[tuple[int]], 
+                         map_list: list[list[str]],
+                         count: int) -> bool:
+        y: int = starting_idx[0]
+        x: int = starting_idx[1]
+
+        curr_char: str = map_list[y][x]
+
+        visited_nodes.add(starting_idx)
+
+        up: tuple[int] = (y-1, x)
+        down: tuple[int] = (y+1, x)
+        left: tuple[int] = (y, x-1)
+        right: tuple[int] = (y, x+1)
+
+        if count == threshold:
+            return True
+        
+        up_cluster: bool = False
+        down_cluster: bool = False
+        left_cluster: bool = False
+        right_cluster: bool = False
+        
+        if (self.promising(curr_char, up, map_list, visited_nodes)):
+            up_cluster = self.identify_cluster(up, threshold, visited_nodes, map_list, count)
+        
+        if (self.promising(curr_char, down, map_list, visited_nodes)):
+            down_cluster = self.identify_cluster(down, threshold, visited_nodes, map_list, count)
+
+        if (self.promising(curr_char, left, map_list, visited_nodes)):
+            left_cluster = self.identify_cluster(left, threshold, visited_nodes, map_list, count)
+
+        if (self.promising(curr_char, right, map_list, visited_nodes)):
+            right_cluster = self.identify_cluster(right, threshold, visited_nodes, map_list, count)
+
+        return up_cluster or down_cluster or left_cluster or right_cluster
+      
+    def clustering(self, threshold: int) -> None:
         map_list: list[list[str]] = self.map_to_list()
         visited_nodes: set[tuple[int]] = set()
+        is_cluster_present: bool = False
 
-        for i, line in enumerate(map_list):
-            for k, char in enumerate(line):
-                if ((i, k) not in visited_nodes):
-                    pass
+        for y, line in enumerate(map_list):
+            for x, char in enumerate(line):
+                curr_idx: tuple[int] = (y, x)
+                count: int = 0
+
+                if (curr_idx not in visited_nodes):
+                    is_cluster_present = self.identify_cluster(curr_idx, threshold, visited_nodes, map_list, count)
+
+                if (is_cluster_present):
+                    break
+
+        print(is_cluster_present)
+
+        if (is_cluster_present):
+            with open('./outputs/day14.txt', 'w') as file:
+                file.write(self)
+                file.close()
         
     def __repr__(self) -> str:
         combined_strs: list[str] = []
@@ -136,6 +206,7 @@ def main() -> None:
         bathroom_map.tick()
         print(f'{bathroom_map}\nSeconds elapsed: {curr_second}\n\n')
         input('Continue?:')
+        bathroom_map.clustering(50)
         curr_second += 1
 
 if __name__ == '__main__':
