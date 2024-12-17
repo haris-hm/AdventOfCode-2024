fab0334bf93b9dd16ffb3df2543c75e19be644d9
haris-hm <haris.mehuljic@drake.edu>
2024-12-17 16:19:52 -0600

    p1 done

--- day16.py
+++ day16.py
@@ -1,138 +1,117 @@
 from collections import defaultdict
 import heapq
 from typing import Self
+from enum import Enum
+
+class Orientation(Enum):
+    NORTH = (-1 ,0)
+    EAST = (0, 1)
+    SOUTH = (1, 0)
+    WEST = (0, -1)
+
+    def rotate(orientation: Self, turn_direction: int) -> Self:
+        orientations: dict[int, Self] = {
+            0: Orientation.NORTH,
+            1: Orientation.EAST,
+            2: Orientation.SOUTH,
+            3: Orientation.WEST
+        }
+
+        orientation_to_index: dict[Self, int] = {
+            Orientation.NORTH: 0,
+            Orientation.EAST: 1,
+            Orientation.SOUTH: 2,
+            Orientation.WEST: 3
+        }
+
+        return orientations[(orientation_to_index[orientation] + turn_direction) % 4]
+        
 
 class Node():
-    def __init__(self, y: int, x: int, direction: str='E') -> None:
+    def __init__(self, y: int, x: int, orientation: Orientation, cost: int | float):
         self.y = y
         self.x = x
-        self.direction = direction
+        self.orientation = orientation
+        self.cost: int | float = cost
 
-    def __lt__(self, other: Self) -> bool:
-        return (self.y, self.x) < (other.y, other.x)
-    
     def __repr__(self) -> str:
-        return f'[({self.y}, {self.x}) | {self.direction}]'
+        return f'[{self.y}, {self.x}, Facing: {self.orientation.name}, Cost: {self.cost}]'
     
-    def __hash__(self) -> int:
-        return hash(self.x*self.y*ord(self.direction[0]))
+    def __eq__(self, other: Self) -> bool:
+        return (self.y, self.x, self.orientation) == (other.y, other.x, other.orientation)
 
-class Graph():
-    def __init__(self, grid: list[list[str]]) -> None:
-        self.adjacency_list: dict[Node, tuple[Node | int]] = defaultdict(list)
+    def __hash__(self) -> bool:
+        return hash((self.y, self.x, self.orientation))
+    
+    def __lt__(self, other: Self) -> bool:
+        return self.cost < other.cost
 
-        self.grid = grid
+class Maze():
+    def __init__(self, grid: str) -> None:
+        self.grid = [[char for char in line] for line in grid.split('\n')]
         self.starting_node: Node = None
-        self.ending_node: Node = None
+        self.ending_nodes: list[Node] = []
 
-    def add_node(self, src: Node, dest: Node, weight: int) -> None:
-        self.adjacency_list[src].insert(0, (dest, weight))
+        for y, line in enumerate(self.grid):
+            for x, char in enumerate(line):
+                if (char == 'S'):
+                    self.starting_node = Node(y, x, Orientation.EAST, 0)
+                elif (char == 'E'):
+                    for orientation in Orientation:
+                        self.ending_nodes.append(Node(y, x, orientation, float('inf')))
 
-    def find_adjacent_nodes(self, src: Node) -> None:
-        count: int = 1
-        for x in range(src.x+1, len(self.grid)):
-            if (self.grid[src.y][x] == '#'):
-                break
-            elif (self.grid[src.y+1][x] == '.' or self.grid[src.y-1][x] == '.'):
-                direction_multiplier: int = 1000 if src.direction != 'E' else 0
-                weight: int = count + direction_multiplier
-                self.add_node(src, Node(src.y, x, 'E'), weight)
-                break
-            count += 1
-
-        count = 1
-        for x in range(src.x-1, 0, -1):
-            if (self.grid[src.y][x] == '#'):
-                break
-            elif (self.grid[src.y+1][x] == '.' or self.grid[src.y-1][x] == '.'):
-                direction_multiplier: int = 1000 if src.direction != 'W' else 0
-                weight: int = count + direction_multiplier
-                self.add_node(src, Node(src.y, x, 'W'), weight)
-                break
-            count += 1
-
-        count = 1
-        for y in range(src.y+1, len(self.grid)):
-            if (self.grid[y][src.x] == '#'):
-                break
-            elif (self.grid[y][src.x+1] == '.' or self.grid[y][src.x-1] == '.'):
-                direction_multiplier: int = 1000 if src.direction != 'S' else 0
-                weight: int = count + direction_multiplier
-                self.add_node(src, Node(y, src.x, 'S'), weight)
-                break
-            count += 1
-
-        count = 1
-        for y in range(src.y-1, 0, -1):
-            if (self.grid[y][src.x] == '#'):
-                break
-            elif (self.grid[y][src.x+1] == '.' or self.grid[y][src.x-1] == '.'):
-                direction_multiplier: int = 1000 if src.direction != 'N' else 0
-                weight: int = count + direction_multiplier
-                self.add_node(src, Node(y, src.x, 'N'), weight)
-                break
-            count += 1
+    def next_nodes(self, node: Node) -> list[Node]:
+        dy, dx = node.orientation.value
+        y_prime, x_prime = node.y+dy, node.x+dx
+        
+        return [
+            Node(y_prime, x_prime, node.orientation, node.cost+1),        
+            Node(node.y, node.x, Orientation.rotate(node.orientation, 1), node.cost+1000),
+            Node(node.y, node.x, Orientation.rotate(node.orientation, -1), node.cost+1000),
+        ]
+    
+    def check_grid(self, y: int, x: int, char: str) -> bool:
+        if (self.grid[y][x] == char):
+            return True
+        return False
 
     def dijkstra(self) -> int:
-        pq: heapq = []
-        heapq.heappush(pq, (0, 0))
+        touch: heapq[Node] = []
+        heapq.heappush(touch, self.starting_node)
 
-        distances: dict[Node, int] = {self.starting_node: 0}
-        visited: set[Node] = set()
+        distances: dict[Node| int] = {self.starting_node: 0}
 
-        nodes: dict[int, Node] = {0: self.starting_node}
-        curr_node_idx: int = 1
+        while touch:
+            curr_node: Node = touch.pop(0)
 
-        while pq:
-            curr_dist, curr_node_key = heapq.heappop(pq)
-            curr_node: Node = nodes[curr_node_key]
-
-            if (curr_node in visited):
-                continue
-            visited.add(curr_node)
-
-            self.find_adjacent_nodes(curr_node)
-
-            if (len(self.adjacency_list[curr_node]) == 0):
-                continue
-
-            for neighbor, weight in self.adjacency_list[curr_node]:
-                if (neighbor in visited):
+            for next_node in self.next_nodes(curr_node):
+                if (self.check_grid(next_node.y, next_node.x, '#')):
                     continue
+               
+                if (next_node not in distances or next_node.cost < distances[next_node]):
+                    distances[next_node] = next_node.cost
+                    heapq.heappush(touch, next_node)
 
-                new_dist = curr_dist + weight
+        ending_variations: list[Node] = [node for node in self.ending_nodes if node in distances]
+        min_distance: int = min([distances[node] for node in ending_variations])
 
-                if (new_dist < distances.get(neighbor, float('inf'))):
-                    distances[neighbor] = new_dist
-                    nodes[curr_node_idx] = neighbor
-                    heapq.heappush(pq, (new_dist, curr_node_idx))
-                    curr_node_idx += 1
-        
-        return distances
+        return min_distance
 
     def __repr__(self) -> str:
         grid_lines: list[str] = [''.join(line) for line in self.grid]
-        grid: str = '\n'.join(grid_lines)
+        grid: str = '      '
+        grid += '\n      '.join(grid_lines)
 
-        return f'Graph:\n   Vertices: {self.adjacency_list}\n   Starting Node: {self.starting_node}\n   Ending Node: {self.ending_node}\n   Grid:\n{grid}'
+        return f'Graph:\n   Starting Node: {self.starting_node}\n   Ending Nodes: {self.ending_nodes}\n   Grid:\n{grid}'
         
 def main() -> None:
-    graph: Graph = None
-    with open('./inputs/day16/0.txt', 'r') as file:
-        contents = file.read()
-        grid: list[list[str]] = [[char for char in line] for line in contents.split('\n')]
-        graph = Graph(grid)
+    maze: Maze = None
+    with open('./inputs/day16/1.txt', 'r') as file:
+        contents: str = file.read()
+        maze = Maze(contents)
 
-        for y, line in enumerate(grid):
-            for x, char in enumerate(line):
-                if (char == 'S'):
-                    graph.starting_node = Node(y, x, 'E')
-                elif (char == 'E'):
-                    graph.ending_node = Node(y, x)
-
-    print(graph)
-    print(graph.dijkstra())
-    print(graph)
+    print(maze)
+    print(f'The minimum cost path is: {maze.dijkstra()}')
 
 if __name__ == '__main__':
     main()
