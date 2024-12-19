e1b6915cdd4ca24f84806f050dd4251b343ac1e4
haris-hm <haris.mehuljic@drake.edu>
2024-12-19 00:40:42 -0600

    day19

--- day19.py
+++ day19.py
@@ -1,32 +1,32 @@
-def promising_branch(design: str, patterns: set[str], pattern_lengths: set[int]) -> bool:
-    is_promising: bool = False
-    for length in pattern_lengths:
-        if design[:length] in patterns:
-            is_promising = True
-            break
-    return is_promising
+def solve(design: str, patterns: list[str], memo: dict[str, int]) -> int:
+    if len(design) == 0:
+        return 1
+    
+    value: int = 0
+    
+    for pattern in patterns:
+        if design[:len(pattern)] == pattern:
+            new_design: str = design[len(pattern):]
+            if new_design in memo:
+                value += memo[new_design]
+            else:
+                solution: int = solve(new_design, patterns, memo)
+                value += solution
+                memo[new_design] = solution
 
-def promising(design: str, pattern: str) -> bool:
-    # print(design[len(pattern):], design[:len(pattern)], pattern)
-    if design[:len(pattern)] == pattern:
-        return True
-    return False
+    return value
 
-def valid_pattern(design: str, patterns: list[str], pattern_lengths: set[int], idx: int=0) -> bool:
-    if promising_branch(design[idx:], set(patterns), pattern_lengths):
-        for i, pattern in enumerate(patterns):
-            if promising(design[idx:], pattern):
-                if idx == len(design)-1:
-                    return True
-                
-                branch: bool = valid_pattern(design, patterns, pattern_lengths, idx+len(pattern))
+def validate_designs(designs: list[str], patterns: list[str]) -> tuple[int]:
+    memo: dict[tuple[str], int] = {}
+    sum: int = 0
+    all_possibilities: int = 0
 
-                if branch:
-                    return branch
-                else:
-                    continue
+    for design in designs:
+        solution: int = solve(design, patterns, memo)
+        sum += 1 if solution > 0 else 0
+        all_possibilities += solution
 
-    return False
+    return sum, all_possibilities
 
 def main() -> None:
     patterns: list[str] = []
@@ -37,20 +37,10 @@ def main() -> None:
         contents: list[str] = file.readlines()
         patterns = [i.rstrip() for i in contents[0].split(', ')]
         designs = [i.rstrip() for i in contents[2:]]
-        for pattern in patterns:
-            pattern_lengths.add(len(pattern))
 
-    print(patterns)
-    print(designs)
-    print(pattern_lengths)
-
-    valid_count: int = 0
-    for i, design in enumerate(designs):
-        valid: bool = valid_pattern(design, patterns, pattern_lengths)
-        valid_count += 1 if valid else 0
-        print(f'Design: {i}/{len(designs)}')
-
-    print(valid_count)
+    possibilites, all_possibilities = validate_designs(designs, patterns)
+    
+    print(f'Possibilities: {possibilites}\nAll Possibilities: {all_possibilities}')
 
 if __name__ == '__main__':
     main()
